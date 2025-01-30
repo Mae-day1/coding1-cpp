483724e542df8add958c9763611352a3a950563c
Mae-day1 <cheyenneenglebright@gmail.com>
2025-01-30 11:56:15 -0600

    Created our first c++ file for Coding 1
    We are learning to use GitHub. Messages and descriptions matter for finding bugs.

---
+++ c1wp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name = "Jinx";
+    int health = 45;
+    cout << "My name is " << name << ".\n";
+    cout << "Hello World!\n";
+}
